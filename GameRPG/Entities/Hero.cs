935d0b231612a532b3ec3cc04e0b209aeb2b01c0
madyfabs <fabriciio_@live.com>
2022-02-22 16:37:57 -0400

    teste

---
+++ GameRPG/Entities/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRPG.Entities
+{
+    internal class Hero
+    {
+
+        public Hero (string Name, int Level, string Type)
+        {
+
+            this.Name = Name;
+            this.Level = Level;
+            this.Type = Type;
+
+        }
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + " "+this.Level + " "+this.Type;
+        }
+
+
+
+    }
+}
---
+++ GameRPG/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GameRPG.Entities;
+
+namespace GameRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero heroi = new Hero("Fabricio", 12, "Knight");
+
+            Console.WriteLine(heroi);
+        }
+    }
+}
